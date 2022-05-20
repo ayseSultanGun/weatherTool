a8f1b1870ec9920f1f12628b6e1a7f3ca73588f9
ayseSultanGun <105431734+ayseSultanGun@users.noreply.github.com>
2022-05-20 09:36:08 -0400

    show current temperature

--- src6/index6.js
+++ src6/index6.js
@@ -1,1 +1,19 @@
-alert("working");
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#mainTemperature");
+  temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°C`;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "df06795b838448a58ab71c48a5044292";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Istanbul&units=metric&appid=${apiKey}`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);

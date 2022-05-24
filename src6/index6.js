6578cb16b65c469f647e10115d11394d4ec87871
ayseSultanGun <105431734+ayseSultanGun@users.noreply.github.com>
2022-05-24 10:12:21 -0400

    added correct forecast data from new API call

--- src6/index6.js
+++ src6/index6.js
@@ -22,6 +22,58 @@ function formatDate(timestamp) {
   return `<strong>${day}</strong>; as of ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+  ];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = "";
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+          <div class="card">
+            <div class="card-body">
+              <img src="https://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png" alt="${
+          forecastDay.weather[0].description
+        }" class="icon" />
+              <p>${formatDay(forecastDay.dt)}</p>
+              <h3>${Math.round(forecastDay.temp.day)}°C</h3>
+            </div>
+          </div>
+        </div>
+`;
+    }
+  });
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "df06795b838448a58ab71c48a5044292";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   celsiusTemperature = response.data.main.temp;
 
@@ -43,7 +95,10 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
+
 function search(city) {
   let apiKey = "df06795b838448a58ab71c48a5044292";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
